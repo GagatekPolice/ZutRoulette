61fd11ca4cbf29656830e18a0e71ff09b071c585
Krakowiak <gagatek_police@wp.pl>
2020-03-03 23:58:55 +0100

    dodanie encji wiadomosci (dev)

--- ZutRuletteAndorid/app/src/main/java/zut/roulette/entity/Message.java
+++ ZutRuletteAndorid/app/src/main/java/zut/roulette/entity/Message.java
@@ -1,4 +1,33 @@
 package zut.roulette.entity;
 
-public class message {
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(tableName = "message_table")
+public class Message {
+
+    @Getter @Setter @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @Getter
+    private int chatId;
+
+    @Getter
+    private int sender;
+
+    @Getter
+    private int receiver;
+
+    @Getter
+    private String message;
+
+    public Message(int chatId, int sender, int receiver, String message) {
+        this.chatId = chatId;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.message = message;
+    }
 }
